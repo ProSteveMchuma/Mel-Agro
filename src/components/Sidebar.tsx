8fdce4221eb0bcd145ace166455b879a33333a4d
ProSteveMchuma <65190415+ProSteveMchuma@users.noreply.github.com>
2026-01-22 14:14:37 +0300

    Update Sidebar.tsx

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import Link from "next/link";
+import { useState, useEffect } from "react";
 import { usePathname, useSearchParams } from "next/navigation";
 
 interface SidebarProps {
