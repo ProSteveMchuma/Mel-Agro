be035414ccf0fe4d21b809296ac250a0ccaf58ad
ProSteveMchuma <65190415+ProSteveMchuma@users.noreply.github.com>
2026-01-30 12:59:08 +0300

    watermark

--- src/components/ProductDetails.tsx
+++ src/components/ProductDetails.tsx
@@ -149,8 +149,8 @@ export default function ProductDetails({ id }: { id: string }) {
                             </div>
 
                             {/* Watermark */}
-                            <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 opacity-10 pointer-events-none z-10 select-none">
-                                <Logo iconOnly className="w-32 h-32" />
+                            <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 opacity-20 pointer-events-none z-10 select-none">
+                                <Logo className="scale-150" />
                             </div>
 
                             <AnimatePresence mode="wait">
